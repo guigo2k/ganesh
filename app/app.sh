ac7964172b45f70a30544c552a95a72d716b19c7
tropicloud <admin@tropicloud.net>
2015-07-28 01:14:33 -0300

    Tue Jul 28 01:14:33 BRT 2015

--- app/app.sh
+++ app/app.sh
@@ -1,26 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# load framework
 . ganesh.sh
 
-# define handlers
-get "/" my_index
-my_index () {
+get "/" app_index
+app_index () {
     header "Content-Type" "text/html"
     cat "index.html"
 }
 
-get "/ps" my_procs
-my_procs () {
+get "/ps" app_proc
+app_proc () {
     header "Content-Type" "text/plain"
     ps aux
 }
 
-get "/redirect" my_redirect
-my_redirect() {
+get "/redirect" app_redirect
+app_redirect() {
     status 302
     header "Location" "https://github.com/"
 }
 
-# run app 
+# that's all
 ganesh_dance
--- app/ganesh.sh
+++ app/ganesh.sh
@@ -1,82 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-CR='\r'
-LF='\n'
-CRLF="$CR$LF"
-http_version="HTTP/1.1"
 ganesh_response="/tmp/ganesh_response"
 
-handle_request () {
-    app="$1"
-
-    # read the request line
-    read request_line
-
-    # read the header lines until we reach a blank line
-    while read header && [ ! "$header" = $'\r' ]; do
-        # FIXME: multiline headers
-        header_name="HTTP_$(echo $header | cut -d ':' -f 1 | tr 'a-z-' 'A-Z_')"
-        export $header_name="$(echo $header | cut -d ':' -f 2 | sed 's/^ //')"
-    done
-
-    # extract HTTP method and HTTP version
-    export REQUEST_METHOD=$(echo $request_line | cut -d ' ' -f 1)
-    export HTTP_VERSION=$(echo $request_line | cut -d ' ' -f 3 | tr -d $'\r')
-
-    # extract the request_path, then PATH_INFO and QUERY_STRING components
-    request_path=$(echo $request_line | cut -d ' ' -f 2)
-    export PATH_INFO=$(echo $request_path | cut -d '?' -f 1)
-    export QUERY_STRING=$(echo $request_path | cut -d '?' -f 2)
-
-    export SCRIPT_NAME=""
-    export SERVER_NAME="localhost"
-    export SERVER_PORT="$port"
-
-    "$app"
-}
-
-handle_response () {
-    response_status="200 OK"
-    response_headers=""
-
-    while read header && [ ! "$header" = "" ]
-    do header_name="$(echo $header | cut -d ':' -f 1 | tr 'A-Z' 'a-z')"
-        
-        if [ "$header_name" = "status" ]
-        then response_status="$(echo $header | cut -d ':' -f 2 | sed 's/^ //')"
-        else
-            if [ "$response_headers" ]
-            then response_headers="$response_headers$CRLF$header"
-            else response_headers="$header"
-            fi
-        fi
-    done
-
-    # Date
-    header="Date: $(date -u '+%a, %d %b %Y %R:%S GMT')"
-    response_headers="$response_headers$CRLF$header"
-
-    # echo status line, headers, blank line, body
-    echo -e "$http_version $response_status$CRLF$response_headers$CRLF$CR"
-    cat
-}
-
+routes_method=()
 routes_path=()
 routes_action=()
-routes_method=()
 
-route () {
+route() {
     routes_method=( ${routes_method[@]} "$1" )
     routes_path=( ${routes_path[@]} "$2" )
     routes_action=( ${routes_action[@]} "$3" )
 }
 
-get () { route "GET" $@; }
-post () { route "POST" $@; }
-delete () { route "DELETE" $@; }
-status () { response_status="$1"; }
+   get() { route "GET" $@ }
+  post() { route "POST" $@ }
+delete() { route "DELETE" $@ }
+status() { response_status="$1" }
 
-header () {
+header() {
     head="$1: $2"
     if [ "$response_headers" ]
     then response_headers="$response_headers\n$head"
@@ -84,7 +25,7 @@ header () {
     fi
 }
 
-not_found () {
+not_found() {
     status "404"
     header "Content-type" "text/plain"
     if [ $# -gt 0 ]
@@ -93,14 +34,14 @@ not_found () {
     fi
 }
 
-reset_response () {
-	response_status="200 OK"
-	response_headers="" 
+reset_response() {
+    response_status="200 OK"
+    response_headers=""
 }
 
-ganesh_dance () {
+ganesh_dance() {
     action=""
-    
+
     for (( i = 0 ; i < ${#routes_method[@]} ; i++ )); do
         method=${routes_method[$i]}
         path=${routes_path[$i]}
@@ -112,7 +53,7 @@ ganesh_dance () {
             fi
         fi
     done
-    
+
     [ ! "$action" ] && action="not_found"
 
     reset_response
@@ -122,7 +63,7 @@ ganesh_dance () {
 
     # set status header, echo headers, blank line, then body
     header "Status" "$response_status"
-    echo -e "$response_headers"
-    echo ""
+    header "Date: `date -u '+%a, %d %b %Y %R:%S GMT'`"
+    echo -e "$response_headers\n"
     cat "$ganesh_response"
 }
